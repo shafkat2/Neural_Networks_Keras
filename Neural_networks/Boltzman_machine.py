25e3c386cc86e4da50a3873c27610878ddca1010
shafkat2 <shafkat.waheed@northsouth.edu>
2019-04-22 00:56:53 +0600

    RBM

--- Neural_networks/Boltzman_machine.py
+++ Neural_networks/Boltzman_machine.py
@@ -23,3 +23,53 @@ test_set_toarray = np.array(test_set, dtype= 'int')
 #getting the number of the users and movies
 nb_user = int(max(max(training_set_toarray[:,0]),max(test_set_toarray[:,0])))
 nb_movies = int(max(max(training_set_toarray[:,1]),max(test_set_toarray[:,1])))
+#converting the data into array with users in line movies in colmuns
+def convert(data):
+    new_data = []
+    for id_users in range(1,nb_user+1):
+        id_movies = data[:,1][data[:,0] == id_users ]
+        id_ratings = data[:,2][data[:,0] == id_users ]
+        ratings = np.zeros(nb_movies)
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
+training_set_to_list  = convert(training_set_toarray )
+test_set_to_list  = convert(test_set_toarray )
+
+#coverting data into tensor
+training_set = torch.Tensor(training_set_to_list)
+test_set = torch.FloatTensor(test_set_to_list)
+
+#rating into binary ratings 1 (Liked) or 0 (Not Liked)
+training_set[training_set == 0] = - 1
+training_set[training_set == 1] = 0
+training_set[training_set == 2] = 0
+training_set[training_set >= 3] = 1
+
+
+test_set[test_set == 0] = - 1
+test_set[test_set == 1] = 0
+test_set[test_set == 2] = 0
+test_set[test_set >= 3] = 1
+
+#creating architecture of the neural network
+
+class RBM():
+    def __init__(self,nv,nh):
+        self.W = torch.randn(nh,nv)
+        self.a = torch.randn(1,nh)
+        self.b = torch.randn(1,nv)
+    def sample_h(self,x):
+        wx = torch.mm(x,self.W.t())
+        activation  = wx + self.a.expand_as(wx)
+        p_h_given_v = torch.sigmoid(activation)
+        return  p_h_given_v, torch.bernoulli(p_h_given_v)
+    def sample_v(self,y):
+        wy = torch.mm(y,self.W)
+        activation  = wy + self.b.expand_as(wy)
+        p_v_given_h = torch.sigmoid(activation)
+        return  p_v_given_h, torch.bernoulli(p_v_given_h)
+    def train(self,v0, vk, ph0, phk):
+         self.W += torch.mm(v0.t(),ph0)-torch.mm(vk.t(),phk) 
+         self.b += torch.sum((v0-vk),0)
+         self.a += torch.sum((ph0 - phk),0)
---
+++ data_analysis/data_analysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+'''downlaod iris.csv from https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv'''
+#Load Iris.csv into a pandas dataFrame.
+iris = pd.read_csv("iris.csv")
