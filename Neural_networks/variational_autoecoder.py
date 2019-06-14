d033f7880874dd5212590d8d1f62c2c89cb392ff
shafkat2 <shafkat.waheed@northsouth.edu>
2019-06-15 05:34:54 +0600

    VAE done

--- Neural_networks/variational_autoecoder.py
+++ Neural_networks/variational_autoecoder.py
@@ -5,11 +5,11 @@ import tensorflow as tf
 
 class ConVAE(object):
     #initializing all the parameter and the varialbe of the conVAE class 
-    def __init__(self,z_size =32,batch_size = 1,learning_rate = 0.0001, kl_tolerance = 3, is_training =false, reuse = false , gpu_model = false ):
+    def __init__(self,z_size =32,batch_size = 1,learning_rate = 0.0001, kl_tolerance = 3, is_training =False, reuse = False , gpu_mode = False ):
         self.z_size = z_size
         self.batch_size = batch_size
         self.learning_rate = learning_rate
-        self.kl_tolearnce = kl_tolearnce
+        self.kl_tolerance = kl_tolerance
         self.is_training = is_training
         self.reuse = reuse
         with tf.variable_scope('conv_vae', reuse = self.reuse):
@@ -33,7 +33,34 @@ class ConVAE(object):
             h = tf.layers.conv2d(self.x,128,4,strides =2,activation = tf.nn.relu, name = "enc_conv3")
             h = tf.layers.conv2d(self.x,256,4,strides =2,activation = tf.nn.relu, name = "enc_conv4")
             h = tf.reshape(h,[-1,2*2*256])
+            #building the "V" part of the VAE
+            self.mu = tf.layers.dense(h, self.z_size, name="enc_fc_mu")
+            self.logvar = tf.layers.dense(h, self.z_size, name="enc_fc_logvar")
+            self.sigma  = tf.exp(self.logvar/2.0)
+            self.epsilon = tf.random_normal([self.batch_size,self.z_size])
+            self.z = self.mu + self.sigma* self.epsilon
+            #building the decoder part of the  VAE
 
+            h = tf.layers.dense(self.z,1024,name = "dec_fc")
+            h = tf.reshape(h,[-1,1,1,1024])
+            h = tf.layers.conv2d_transpose(h,128,5,strides =2,activation = tf.nn.relu, name= "dec_decon1")
+            h = tf.layers.conv2d_transpose(h,64,5,strides =2,activation = tf.nn.relu, name= "dec_decon2")
+            h = tf.layers.conv2d_transpose(h,32,6,strides =2,activation = tf.nn.relu, name= "dec_decon3")
+            self.y = tf.layers.conv2d_transpose(h,3,4,strides =2,activation = tf.sigmoid, name= "dec_decon4")
+            # training operations
+            if self.is_training:
+                self.global_step = tf.Variable(0, name='global_step', trainable=False)
+                self.r_loss = tf.reduce_sum(tf.square(self.x - self.y), reduction_indices = [1,2,3])
+                self.r_loss = tf.reduce_mean(self.r_loss)
+                self.kl_loss = - 0.5 * tf.reduce_sum((1 + self.logvar - tf.square(self.mu) - tf.exp(self.logvar)), reduction_indices = 1)
+                self.kl_loss = tf.maximum(self.kl_loss, self.kl_tolerance * self.z_size)
+                self.kl_loss = tf.reduce_mean(self.kl_loss)
+                self.loss = self.r_loss + self.kl_loss
+                self.lr = tf.Variable(self.learning_rate, trainable=False)
+                self.optimizer = tf.train.AdamOptimizer(self.lr)
+                grads = self.optimizer.compute_gradients(self.loss)
+                self.train_op = self.optimizer.apply_gradients(grads, global_step=self.global_step, name='train_step')
+            self.init = tf.global_variables_initializer()
 
 
 
